66cb27ab51c558821538a136bbcbf8512fd7e57c
Ariefsetiawan900 <ariefsetiawan8788@gmail.com>
2021-06-24 16:00:45 +0700

    first commit -m

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./pages/Home";
+import EditProducts from "./pages/EditProducts";
+import AddProducts from "./pages/AddProducts";
 
-function App() {
+import Navbar from './components/Navbar'
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+    
+      <Navbar/>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/products/add" component={AddProducts} />
+        <Route exact path="/products/edit/:id" component={EditProducts} />
+      </Switch>
+      
+    </Router>
   );
-}
+};
 
 export default App;
---
+++ src/pages/Home.js
@@ -0,0 +1,72 @@
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+
+const Home = () => {
+  const [users, setUsers] = useState([]);
+
+  const loadUsers = async () => {
+    try {
+      const result = await axios.get("https://27dfda22cc14.ngrok.io/karyawan");
+      console.log(result.data.data);
+      setUsers(result.data.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const deleteUser = async (nik) => {
+    try {
+      await axios.delete(`https://27dfda22cc14.ngrok.io/karyawan/${nik}`);
+
+      setUsers(users.filter((user) => user.nik !== nik));
+      alert("Data berhasil dihapus");
+    } catch (error) {
+      alert("gagal untuk menghapus");
+    }
+  };
+
+  useEffect(() => {
+    loadUsers();
+  }, []);
+  return (
+    <div className="container">
+      <div className="py-4">
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col">No</th>
+              <th scope="col">Nama</th>
+              <th scope="col">No. Handphone</th>
+              <th scope="col">Email</th>
+              <th scope="col">Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user, index) => (
+              <tr>
+                <th scope="row">{index + 1}</th>
+                <td>{user.name}</td>
+                <td>{user.phone}</td>
+                <td>{user.email}</td>
+                <td>
+                  <Link style={{ marginRight: "10px" }}>
+                    <i class="bi bi-pencil-square text-warning fs-4"></i>
+                  </Link>
+                  <Link>
+                    <i
+                      class="bi bi-trash-fill text-danger fs-4"
+                      onClick={() => deleteUser(user.nik)}
+                    ></i>
+                  </Link>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
